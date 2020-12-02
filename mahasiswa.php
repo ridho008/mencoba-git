677d060b2cff050659a473c7544f42a202b7150b
ridho008 <ciuyy12@gmail.com>
2020-12-02 18:50:32 +0700

    perbarui hal mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,1 +1,11 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mahasiswa</title>
+</head>
+<body>
+	<h1>Data Mahasiswa</h1>
+</body>
+</html>
